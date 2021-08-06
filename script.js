6b1008781bada2add7b9fb9150905066962d2056
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-06 18:11:56 -0400

    JS event's

---
+++ script.js
@@ -0,0 +1,39 @@
+const footer1 = document.getElementById("footer-section1");
+
+footer1.addEventListener("mouseover", function(event) {
+    event.target.style.color = "purple";
+}, false); 
+
+footer1.addEventListener("mouseout", function(event) {
+    event.target.style.color = "";
+}, false);
+
+const footer2 = document.getElementById("footer-section2");
+
+footer2.addEventListener("mouseover", function(event) {
+    event.target.style.color = "orange";
+}, false); 
+
+footer2.addEventListener("mouseout", function(event) {
+    event.target.style.color = "";
+}, false);
+
+const footer3 = document.getElementById("footer-section3");
+
+footer3.addEventListener("mouseover", function(event) {
+    event.target.style.color = "red";
+}, false); 
+
+footer3.addEventListener("mouseout", function(event) {
+    event.target.style.color = "";
+}, false);
+
+const footer4 = document.getElementById("footer-section4");
+
+footer4.addEventListener("mouseover", function(event) {
+    event.target.style.color = "blue";
+}, false); 
+
+footer4.addEventListener("mouseout", function(event) {
+    event.target.style.color = "";
+}, false);
